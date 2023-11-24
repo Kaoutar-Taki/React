3515be85d3357f35c457f031a9d4190f07de0306
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-24 16:25:30 +0100

    JSX Rules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+function Greeting() {
+  return (
+    <>
+      <div onClick={() => console.log("clicked")}>
+        <h1 className="">Hello World</h1>
+        <Person />
+        <p>This is my message</p>
+        <ul>
+          <li>
+            <a href="www.home.com">Home</a>
+          </li>
+          <img src="" alt="" />
+          <input type="text" name="" id="" />
+        </ul>
+      </div>
+    </>
+  );
+}
+const Person = () => <h2>Kaoutar Taki</h2>;
+
 // function Greeting() {
 //   return (
 //     <div>
@@ -9,8 +29,8 @@ import ReactDom from "react-dom";
 //   );
 // }
 
-const Greeting = () => {
-  return React.createElement('div',{},React.createElement('h2',{},'Hello World'));
-}
+// const Greeting = () => {
+//   return React.createElement('div',{},React.createElement('h2',{},'Hello World'));
+// }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
