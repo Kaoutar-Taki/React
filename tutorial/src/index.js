3ab2566c037f44fa956b8602b5cc0b881291a2ef
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-24 12:33:06 +0100

    First Component

---
+++ tutorial/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+const Greeting = () => {
+  return React.createElement('div',{},React.createElement('h2',{},'Hello World'));
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
