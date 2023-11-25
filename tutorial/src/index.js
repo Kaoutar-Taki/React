dc01d151f9cd6c10712e1bb8e8b4d3bc1b13eaac
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-25 12:49:16 +0100

    Props List

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,120 +2,127 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+const books = [
+  {
+    id: 1,
+    Title: "The Silent Patient",
+    Author: "Alex Michaelides",
+    Genre: "Psychological Thriller",
+    Price: 25.0,
+    Country: "United States",
+    img: "img/book5.jpg",
+  },
+  {
+    id: 2,
+    Title: "Educated",
+    Author: "Tara Westover",
+    Genre: "Memoir",
+    Price: 18.99,
+    Country: "United States",
+    img: "img/book3.jpg",
+  },
+  {
+    id: 3,
+    Title: "Sapiens: A Brief History of Humankind",
+    Author: "Yuval Noah Harari",
+    Genre: "Non-Fiction",
+    Price: 19.99,
+    Country: "Israel",
+    img: "img/book9.jpg",
+  },
+  {
+    id: 4,
+    Title: "Dune",
+    Author: "Frank Herbert",
+    Genre: "Science Fiction",
+    Price: 15.5,
+    Country: "United States",
+    img: "img/book.jpg",
+  },
+  {
+    id: 5,
+    Title: "To Kill a Mockingbird",
+    Author: "Harper Lee",
+    Genre: "Fiction",
+    Price: 14.99,
+    Country: "United States",
+    img: "img/book7.jpg",
+  },
+  {
+    id: 6,
+    Title: "To Kill a Mockingbird",
+    Author: "Harper Lee",
+    Genre: "Fiction",
+    Price: 12.99,
+    Country: "United States",
+    img: "img/book6.jpg",
+  },
+  {
+    id: 7,
+    Title: "Dune",
+    Author: "Frank Herbert",
+    Genre: "Science Fiction",
+    Price: 18.5,
+    Country: "United States",
+    img: "img/book2.jpg",
+  },
+  {
+    id: 8,
+    Title: "Educated",
+    Author: "Tara Westover",
+    Genre: "Memoir",
+    Price: 14.95,
+    Country: "United States",
+    img: "img/book5.jpg",
+  },
+  {
+    id: 9,
+    Title: "The Da Vinci Code",
+    Author: "Dan Brown",
+    Genre: "Mystery/Thriller",
+    Price: 10.99,
+    Country: "United States",
+    img: "img/book4.jpg",
+  },
+  {
+    id: 10,
+    Title: "Sapiens: A Brief History of Humankind",
+    Author: "Yuval Noah Harari",
+    Genre: "Non-Fiction",
+    Price: 20.0,
+    Country: "United Kingdom",
+    img: "img/book3.jpg",
+  },
+  {
+    id: 11,
+    Title: "The Great Gatsby",
+    Author: "F. Scott Fitzgerald",
+    Genre: "Fiction",
+    Price: 9.99,
+    Country: "United States",
+    img: "img/book.jpg",
+  },
+];
 function BookList() {
   return (
     <section className="book-list">
-      <Book
-        Title="The Silent Patient"
-        Author="Alex Michaelides"
-        Genre="Psychological Thriller"
-        Price="25.00"
-        Country="United States"
-        img={img5}
-      />
-      <Book
-        Title="Educated"
-        Author="Tara Westover"
-        Genre="Memoir"
-        Price="18.99"
-        Country="United States"
-        img={img3}
-      />
-      <Book
-        Title="Sapiens: A Brief History of Humankind"
-        Author="Yuval Noah Harari"
-        Genre="Non-Fiction"
-        Price="19.99"
-        Country="Israel"
-        img={img9}
-      />
-      <Book
-        Title="Dune"
-        Author="Frank Herbert"
-        Genre="Science Fiction"
-        Price="15.50"
-        Country="United States"
-        img={img8}
-      />
-      <Book
-        Title="To Kill a Mockingbird"
-        Author="Harper Lee"
-        Genre="Fiction"
-        Price="14.99"
-        Country="United States"
-        img={img7}
-      />
-      <Book
-        Title="To Kill a Mockingbird"
-        Author="Harper Lee"
-        Genre="Fiction"
-        Price="12.99"
-        Country="United States"
-        img={img6}
-      />
-      <Book
-        Title="Dune"
-        Author="Frank Herbert"
-        Genre="Science Fiction"
-        Price="18.50"
-        Country="United States"
-        img={img2}
-      />
-      <Book
-        Title="Educated"
-        Author="Tara Westover"
-        Genre="Memoir"
-        Price="14.95"
-        Country="United States"
-        img={img5}
-      />
-      <Book
-        Title="The Da Vinci Code"
-        Author="Dan Brown"
-        Genre="Mystery/Thriller"
-        Price="10.99"
-        Country="United States"
-        img={img4}
-      />
-      <Book
-        Title="Sapiens: A Brief History of Humankind"
-        Author="Yuval Noah Harari"
-        Genre="Non-Fiction"
-        Price="20.00"
-        Country="United Kingdom"
-        img={img3}
-      />
-      <Book
-        Title="The Great Gatsby"
-        Author="F. Scott Fitzgerald"
-        Genre="Fiction"
-        Price="9.99"
-        Country="United States"
-        img={img}
-      />
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 const Book = (props) => {
+  const { img, Title, Genre, Author, Country, Price } = props;
   return (
     <article className="book">
-      <img src={props.img} alt="" width={250} height={300} />
-      <h2>{props.Title}</h2>({props.Genre})<h4>{props.Author}</h4>
-      <h5>{props.Country.toLowerCase()}</h5>
-      <p>{props.Price} $</p>
+      <img src={img} alt="" width={250} height={300} />
+      <h2>{Title}</h2>({Genre})<h4>{Author}</h4>
+      <h5>{Country.toLowerCase()}</h5>
+      <p>{Price} $</p>
     </article>
   );
 };
-
-const img = "img/book.jpg";
-const img2 = "img/book2.jpg";
-const img3 = "img/book3.jpg";
-const img4 = "img/book4.jpg";
-const img5 = "img/book5.jpg";
-const img6 = "img/book6.jpg";
-const img7 = "img/book7.jpg";
-const img8 = "img/book8.jpg";
-const img9 = "img/book9.jpg";
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 
