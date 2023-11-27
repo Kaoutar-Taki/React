ec66cb0adc8c0097c14a065eb7976992c53b7ef6
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-27 11:29:29 +0100

    1-useState/3-useState-array

--- react-advanced/src/data.js
+++ react-advanced/src/data.js
@@ -1,6 +1,8 @@
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
+  { id: 1, name: "john", age: 28, city: "New York" },
+  { id: 2, name: "peter", age: 35, city: "San Francisco" },
+  { id: 3, name: "susan", age: 30, city: "Los Angeles" },
+  { id: 4, name: "anna", age: 25, city: "Chicago" },
+  { id: 5, name: "michael", age: 32, city: "Miami" },
+  { id: 6, name: "emily", age: 29, city: "Seattle" },
 ];
--- react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+import { React, useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setpeople] = useState(data);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setpeople(newPeople);
+  };
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, city } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <h5>{age} old</h5>({city})
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button type="button" className="btn" onClick={() => setpeople([])}>
+        Clear items
+      </button>
+      <button type="button" className="btn" onClick={() => setpeople(data)}>
+        Restor items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
