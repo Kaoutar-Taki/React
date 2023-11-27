c55a2b248c6aad87163b30696187fc56c5d5d221
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-27 12:10:28 +0100

    1-useState/4-useState-object

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Setup from "./tutorial/1-useState/setup/2-useState-basics";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
+// import Setup from "./tutorial/1-useState/setup/3-useState-array";
+import Setup from "./tutorial/1-useState/setup/4-useState-object";
 
 function App() {
   return (
     <div className="container">
-      {/* <Setup /> */}
-      {/* <Setup /> */}
       <Setup />
     </div>
   );
--- react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const name = "Kaoutar";
+  const age = 20;
+  const msg = "Random Message";
+
+  const [person, setPerson] = useState({ name, age, msg });
+  const chageMsg = () => {
+    setPerson({ ...person, msg: "Hello World" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h2>{person.age}</h2>
+      <h4>{person.msg}</h4>
+      <button className="btn" onClick={chageMsg}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
