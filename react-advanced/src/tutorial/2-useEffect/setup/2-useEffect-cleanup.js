bc8f90f0b5cd7dd1fea041fc0fd05d518422b20f
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-27 21:43:00 +0100

    2-useEffect/2-useEffect-cleanup

--- react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerHeight);
+  const checkSize = () => {
+    setSize(window.innerHeight);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  });
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
