1a1f38b582bc20fe923b78be3cb45aa34157137e
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-28 10:30:21 +0100

    3-conditional-rendering/1-multiple-returns

--- react-advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,63 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users/QuincyLarson";
+const accessToken = "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"; // Replace with your actual GitHub access token
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((userData) => {
+        const { login, id } = userData;
+        setUser({ login, id });
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error....</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{user.login}</h1>
+      <img
+        src="https://avatars.githubusercontent.com/u/985197?v=4"
+        alt="avatar"
+      />
+      <h2>{user.id}</h2>
+      <a href="https://www.freeCodeCamp.org">Bio</a>
+    </div>
+  );
 };
 
 export default MultipleReturns;
