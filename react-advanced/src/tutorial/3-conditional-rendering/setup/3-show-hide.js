cb0c70b18fa64b1d3d7e33e4528d0811f1d83799
Kaoutar-Taki <takikaoutar01@gmail.com>
2023-11-28 12:06:53 +0100

    3-conditional-rendering/3-show-hide

--- react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const [textHide, setTextHide] = useState("salse");
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  });
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  const chowHide = () => {
+    setTextHide(!textHide);
+  };
+
+  return (
+    <>
+      <button type="button" className="btn" onClick={chowHide}>
+        {textHide ? "Show" : "Hide"}
+      </button>
+      {!textHide && (
+        <div style={{ marginTop: "2rem" }}>
+          <h1>window</h1>
+          <h2>Size : {size}</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShowHide;
